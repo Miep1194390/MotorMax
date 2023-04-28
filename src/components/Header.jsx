1d745bcb4949a1fd1f51f97955e1990be6f76ff9
ililuab <1194420@student.roc-nijmegen.nl>
2023-04-28 11:54:47 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/App.scss";
+
+const Header = () => {
+    return (
+        <div>
+            <h1><p>header</p></h1>
+        </div>
+    );
+}
+    
+export default Header;
