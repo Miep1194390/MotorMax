04e297ff3b89c1d4d853533bfb8604c5416ba719
ililuab <1194420@student.roc-nijmegen.nl>
2023-04-28 12:26:52 +0200

    scss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "../css/App.scss";
+import logo from "../img/logo2.png";
 
 const Header = () => {
     return (
         <div>
-            <h1><p>header</p></h1>
+            <div className="header-container d-flex justify-content-center">
+                <div className="logo">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="login"></div>
+            </div>
         </div>
-    );
+    );  
 }
     
 export default Header;
