4ad00d4f7a99968070994bf27c51aabf4bbd5e5d
Miep <91285462+Miep1194390@users.noreply.github.com>
2023-05-11 12:43:24 +0200

    Signup mee bezig

--- src/firebase.js
+++ src/firebase.js
@@ -21,8 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
-
-export default { app, analytics, auth, provider };
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
