a78006fa155c34464973107302c3413f83fedef5
Miep <91285462+Miep1194390@users.noreply.github.com>
2023-05-11 12:03:49 +0200

    login mee bezig

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,36 +4,13 @@ import Header from "./Header.jsx";
 
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  function handleLogin(event) {
-    event.preventDefault();
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        // Logged in successfully
-      })
-      .catch((error) => {
-        // Error occurred
-      });
-  }
-
   return (
     <div>
       <Header></Header>
-    <form onSubmit={handleLogin}>
-      <label>Email:</label>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
 
-      <label>Password:</label>
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+    </div>
+  )
 
-      <button type="submit">Log in</button>
-      </form>
-      </div>
-  );
 }
 
 export default Login;
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from '@firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,17 +23,6 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-const handleLogin = (email, password) => {
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      console.log("User logged in:", user.email);
-    })
-    .catch((error) => {
-      console.error("Error logging in:", error);
-    });
-}
-
-export { app, analytics, auth };
-export default handleLogin;
+export default { app, analytics, auth, provider };
