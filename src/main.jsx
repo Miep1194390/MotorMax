ce3f827fea92f41a418a1d163a3df8c66569a946
Miep <mirzaselimovic2005@gmail.com>
2023-06-01 11:23:31 +0200

    - Private Route, Routing Fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import App from "./App.jsx";
 import Hoewerkthet from "./components/Hoewerkthet.jsx";
 import Login from "./components/Login.jsx";
 import Admin from "./components/Admin.jsx";
 import Feed from "./components/Feed.jsx";
 import Settings from "./components/Settings.jsx";
-import PrivateRoute from "../src/components/PrivateRoute.jsx";
 
 const Root = () => {
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
   return (
     <React.StrictMode>
@@ -22,12 +20,7 @@ const Root = () => {
           <Route path="/admin" element={<Admin />} />
           <Route path="/login" element={<Login />} />
           <Route path="/feed" element={<Feed />} />
-          <PrivateRoute
-            path="/settings"
-            element={<Settings />}
-            isAuthenticated={isAuthenticated}
-          />
-          <Route path="*" element={<h1>Not Found</h1>} />
+          <Route path="/settings" element={<Settings />} />
         </Routes>
       </BrowserRouter>
     </React.StrictMode>
