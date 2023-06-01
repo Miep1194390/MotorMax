ceadd6c09f3193be24912bdafcc1b8ea03d644e2
Miep <mirzaselimovic2005@gmail.com>
2023-06-01 11:19:53 +0200

    Settings Routes, Settings Page

---
+++ src/components/Settings.jsx
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import { auth, db } from "../firebase";
+import { doc, updateDoc, getDoc, setDoc } from "firebase/firestore";
+import { updateProfile } from "firebase/auth";
+
+const Settings = () => {
+  const [displayName, setDisplayName] = useState("");
+  const [isPrivate, setIsPrivate] = useState(false);
+
+  useEffect(() => {
+    const user = auth.currentUser;
+    if (user) {
+      setDisplayName(user.displayName);
+      const userRef = doc(db, "users", user.uid);
+      const fetchData = async () => {
+        const docSnap = await getDoc(userRef);
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          setIsPrivate(data.isPrivate);
+        }
+      };
+      fetchData();
+    }
+  }, []);
+
+  const saveSettings = async () => {
+    const user = auth.currentUser;
+
+    if (user) {
+      try {
+        await updateProfile(user, { displayName });
+        console.log("Naam aangepast.");
+
+        const userRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(userRef);
+
+        if (docSnap.exists()) {
+          await updateDoc(userRef, { isPrivate });
+          console.log("Privacy instellingen aangepast.");
+        } else {
+          const newUser = {
+            displayName: user.displayName,
+            isPrivate,
+          };
+          await setDoc(userRef, newUser);
+          console.log("Maakt nieuwe gebruiker aan in db.");
+        }
+      } catch (error) {
+        console.error("Error met updaten van instellingen:", error);
+      }
+    }
+  };
+
+  return (
+    <div className="settings-page">
+      <h1 className="settings-page__title">Instellingen</h1>
+      <div className="settings-page__form-group">
+        <label htmlFor="displayName" className="settings-page__label">Gebruikersnaam:</label>
+        <input
+          type="text"
+          id="displayName"
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+          className="settings-page__input"
+        />
+      </div>
+      <div className="settings-page__form-group">
+        <label htmlFor="isPrivate" className="settings-page__label">Maak account prive:</label>
+        <input
+          type="checkbox"
+          id="isPrivate"
+          checked={isPrivate}
+          onChange={(e) => setIsPrivate(e.target.checked)}
+          className="settings-page__checkbox"
+        />
+      </div>
+      <button onClick={saveSettings} className="settings-page__button">Opslaan</button>
+    </div>
+  );
+};
+
+export default Settings;
--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Hoewerkthet from './components/Hoewerkthet.jsx';
-import Login from './components/Login.jsx';
-import Admin from './components/Admin.jsx'
-import Feed from './components/Feed.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import App from "./App.jsx";
+import Hoewerkthet from "./components/Hoewerkthet.jsx";
+import Login from "./components/Login.jsx";
+import Admin from "./components/Admin.jsx";
+import Feed from "./components/Feed.jsx";
+import Settings from "./components/Settings.jsx";
+import PrivateRoute from "../src/components/PrivateRoute.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/hoewerkthet" element={<Hoewerkthet />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/feed" element={<Feed />}></Route>
-        <Route path="*" element={<h1>Not Found</h1>} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+const Root = () => {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/hoewerkthet" element={<Hoewerkthet />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/feed" element={<Feed />} />
+          <PrivateRoute
+            path="/settings"
+            element={<Settings />}
+            isAuthenticated={isAuthenticated}
+          />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Root />);
