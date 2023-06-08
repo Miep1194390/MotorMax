95b17f08e2c6eb221d59a75eca9faed04f56b3be
Miep <mirzaselimovic2005@gmail.com>
2023-06-08 12:25:00 +0200

    Settings Front End

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { auth, provider } from "../firebase";
 import { signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const Login = () => {
   const [username, setUsername] = useState("");
@@ -29,8 +31,25 @@ const Login = () => {
         console.log("Gebruikersinformatie:", user);
         localStorage.setItem("username", user.displayName);
         localStorage.setItem("email", user.email);
-        navigateTo("/feed")
+        navigateTo("/feed");
 
+        const db = getFirestore();
+        const usersCollection = collection(db, "users");
+        const userDoc = doc(usersCollection, user.uid);
+
+        const userData = {
+          displayName: user.displayName,
+          email: user.email,
+          // Add any additional user data you want to store in Firestore
+        };
+
+        setDoc(userDoc, userData)
+          .then(() => {
+            console.log("User document created in Firestore");
+          })
+          .catch((error) => {
+            console.error("Error creating user document in Firestore:", error);
+          });
       })
       .catch((error) => {
         console.error("Fout bij aanmelden:", error);
@@ -50,29 +69,43 @@ const Login = () => {
 
   return (
     <div className="d-flex align-items-center vh vw">
-    <div className="container-fluid" >
-      <div className="row">
-        <div className="content-right col-6 d-flex justify-content-center">
-          <img src="../src\img\logo1.png" alt="" />
-        </div>
-        <div className="content-left col-6 d-flex justify-content-center flex-column">
-          <h1 className="text-center">MotorMax</h1>
-          <br />
-          <button className="" onClick={() => handleClick(provider)}><p>Inloggen met Google</p></button>
-          <br />
-          <button className="" onClick={() => handleClick(provider)}><p>Inloggen met Microsoft</p></button>
-          <br />
-          <hr />
-          <p className="p-0 m-0">Geen account?</p><br />
-          <button className="" onClick={handleClick}><p>Registreren met google</p></button>
-          <br />
-          <button className="" onClick={handleClick}><p>Registreren met Microsoft</p></button>
-          <br />
-          <p className="p-0 m-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci praesentium laborum enim reiciendis recusandae laboriosam suscipit molestias! Earum nobis, quo blanditiis magnam est magni explicabo quam tempora deleniti, natus illo?</p>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="content-right col-6 d-flex justify-content-center">
+            <img src="../src\img\logo1.png" alt="" />
+          </div>
+          <div className="content-left col-6 d-flex justify-content-center flex-column">
+            <h1 className="text-center">MotorMax</h1>
+            <br />
+            <button className="" onClick={() => handleClick(provider)}>
+              <p>Inloggen met Google</p>
+            </button>
+            <br />
+            <button className="" onClick={() => handleClick(provider)}>
+              <p>Inloggen met Microsoft</p>
+            </button>
+            <br />
+            <hr />
+            <p className="p-0 m-0">Geen account?</p>
+            <br />
+            <button className="" onClick={handleClick}>
+              <p>Registreren met Google</p>
+            </button>
+            <br />
+            <button className="" onClick={handleClick}>
+              <p>Registreren met Microsoft</p>
+            </button>
+            <br />
+            <p className="p-0 m-0">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci
+              praesentium laborum enim reiciendis recusandae laboriosam suscipit
+              molestias! Earum nobis, quo blanditiis magnam est magni explicabo
+              quam tempora deleniti, natus illo?
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 };
 
--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -6,6 +6,7 @@ import { updateProfile } from "firebase/auth";
 const Settings = () => {
   const [displayName, setDisplayName] = useState("");
   const [isPrivate, setIsPrivate] = useState(false);
+  const [feedbackMessage, setFeedbackMessage] = useState("");
 
   useEffect(() => {
     const user = auth.currentUser;
@@ -25,58 +26,77 @@ const Settings = () => {
 
   const saveSettings = async () => {
     const user = auth.currentUser;
-  
+
     if (user) {
       try {
         await updateProfile(user, { displayName });
-  
+
         const userRef = doc(db, "users", user.uid);
         const docSnap = await getDoc(userRef);
-  
+
         if (docSnap.exists()) {
-          await updateDoc(userRef, { isPrivate });
+          await updateDoc(userRef, {
+            displayName: displayName,
+            isPrivate,
+          });
           console.log("Privacy instellingen aangepast.");
+          setFeedbackMessage("Instellingen succesvol opgeslagen!");
         } else {
           const newUser = {
-            displayName,
+            displayName: displayName,
             isPrivate,
           };
           await setDoc(userRef, newUser);
           console.log("Maakt nieuwe gebruiker aan in db.");
+          setFeedbackMessage("Instellingen succesvol opgeslagen!");
         }
-  
-        setDisplayName(displayName); // Update the state after the profile is updated
+
+        setDisplayName(displayName);
         console.log("Naam aangepast.");
       } catch (error) {
         console.error("Error met updaten van instellingen:", error);
+        setFeedbackMessage(
+          "Er is een fout opgetreden bij het opslaan van de instellingen."
+        );
       }
     }
-  };  
+  };
 
   return (
     <div className="settings-page">
-      <h1 className="settings-page__title">Instellingen</h1>
-      <div className="settings-page__form-group">
-        <label htmlFor="displayName" className="settings-page__label">Gebruikersnaam:</label>
-        <input
-          type="text"
-          id="displayName"
-          value={displayName}
-          onChange={(e) => setDisplayName(e.target.value)}
-          className="settings-page__input"
-        />
+      <div className="settings-page-inner">
+        <h1 className="settings-page__title">Profiel bewerken</h1>
+        <div className="settings-page__form-group">
+          <label htmlFor="displayName" className="settings-page__label">
+            Gebruikersnaam:
+          </label>
+          <input
+            type="text"
+            id="displayName"
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
+            className="settings-page__input"
+          />
+        </div>
+        <div className="settings-page__form-group">
+          <label htmlFor="isPrivate" className="settings-page__label">
+            Maak account prive:
+          </label>
+          <input
+            type="checkbox"
+            id="isPrivate"
+            checked={isPrivate}
+            onChange={(e) => setIsPrivate(e.target.checked)}
+            className="settings-page__checkbox"
+          />
+        </div>
+        <button onClick={saveSettings} className="settings-page__button">
+          Verzenden
+        </button>
+        {feedbackMessage && (
+          <p className="settings-page__feedback">{feedbackMessage}</p>
+        )}
       </div>
-      <div className="settings-page__form-group">
-        <label htmlFor="isPrivate" className="settings-page__label">Maak account prive:</label>
-        <input
-          type="checkbox"
-          id="isPrivate"
-          checked={isPrivate}
-          onChange={(e) => setIsPrivate(e.target.checked)}
-          className="settings-page__checkbox"
-        />
-      </div>
-      <button onClick={saveSettings} className="settings-page__button">Opslaan</button>
     </div>
   );
 };
