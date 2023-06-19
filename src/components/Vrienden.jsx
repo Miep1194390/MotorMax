897b192106315d396c27b4b2dfb11f313a05b965
Miep <mirzaselimovic2005@gmail.com>
2023-06-19 12:14:58 +0200

    Vrienden

--- src/components/Vrienden.jsx
+++ src/components/Vrienden.jsx
@@ -1,16 +1,24 @@
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore, collection, query, where, getDocs, addDoc } from 'firebase/firestore';
-import { useState } from 'react';
-import { db } from '../firebase';
-import { Link } from 'react-router-dom';
-
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+  addDoc,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
+import { useState, useEffect } from "react";
+import { db } from "../firebase";
+import { Link } from "react-router-dom";
 
 function Vrienden() {
   const [searchInput, setSearchInput] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [friendRequests, setFriendRequests] = useState([]);
 
   const user = localStorage.getItem("uid");
 
@@ -18,8 +26,11 @@ function Vrienden() {
   const searchFirestore = async () => {
     setSearchResults([]);
 
-    const q = query(collection(db, "users"), where("displayName", "==", searchInput));
-    
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", searchInput)
+    );
+
     try {
       const querySnapshot = await getDocs(q);
       querySnapshot.forEach((doc) => {
@@ -34,11 +45,9 @@ function Vrienden() {
   };
 
   const sendFriendRequest = async (recipientId) => {
-
-    // set uid into const senderId
     const senderId = user;
-    const status = "pending"; 
-    
+    const status = "pending";
+
     try {
       await addDoc(collection(db, "friendRequests"), {
         recipientId,
@@ -51,6 +60,58 @@ function Vrienden() {
     }
   };
 
+  const fetchFriendRequests = async () => {
+    const q = query(
+      collection(db, "friendRequests"),
+      where("recipientId", "==", user)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      const requests = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setFriendRequests(requests);
+    } catch (error) {
+      console.log("Error fetching friend requests:", error);
+    }
+  };
+
+  const acceptFriendRequest = async (request) => {
+    const requestDocRef = doc(db, "friendRequests", request.id);
+
+    try {
+      await updateDoc(requestDocRef, { status: "accepted" });
+      console.log("Friend request accepted successfully");
+      // Optionally, you can remove the request from the friendRequests state.
+      setFriendRequests((prevRequests) =>
+        prevRequests.filter((r) => r.id !== request.id)
+      );
+    } catch (error) {
+      console.log("Error accepting friend request:", error);
+    }
+  };
+
+  const denyFriendRequest = async (request) => {
+    const requestDocRef = doc(db, "friendRequests", request.id);
+
+    try {
+      await updateDoc(requestDocRef, { status: "denied" });
+      console.log("Friend request denied successfully");
+      // Optionally, you can remove the request from the friendRequests state.
+      setFriendRequests((prevRequests) =>
+        prevRequests.filter((r) => r.id !== request.id)
+      );
+    } catch (error) {
+      console.log("Error denying friend request:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchFriendRequests();
+  }, []);
+
   return (
     <div className="container-fluid maak-container">
       <div className="row">
@@ -58,31 +119,44 @@ function Vrienden() {
           <div className="sidebar-vrienden">
             <Link to="/">TERUG</Link>
             <input
-              className='mt-4'
+              className="mt-4"
               type="text"
               value={searchInput}
               onChange={(e) => setSearchInput(e.target.value)}
               placeholder="Search..."
-              />
-              <button onClick={searchFirestore} className='mt-3' >Search</button>
-            </div>
+            />
+            <button onClick={searchFirestore} className="mt-3">
+              Search
+            </button>
           </div>
-          
-
-            <div className="col-xxl-9 col-xl-9 col-lg-9 col-md-9">
-              <div className='meetings'>
-                {searchResults.map((result) => (
-                  <div key={result.id}>
-                    <p>Name: {result.displayName}</p>
-                    <p>Email: {result.email}</p>
-                    <button onClick={() => sendFriendRequest(result.id)}>Send Friend Request</button>
-                    <hr />
-                  </div>
-                ))}
+        </div>
+        <div className="col-xxl-9 col-xl-9 col-lg-9 col-md-9">
+          <div className="meetings">
+            {searchResults.map((result) => (
+              <div key={result.id}>
+                <p>Name: {result.displayName}</p>
+                <p>Email: {result.email}</p>
+                <button onClick={() => sendFriendRequest(result.id)}>
+                  Send Friend Request
+                </button>
+                <hr />
               </div>
+            ))}
+          </div>
+
+          {friendRequests.map((request) => (
+            <div key={request.id}>
+              <p>Name: {request.senderId}</p>
+              <button onClick={() => acceptFriendRequest(request)}>
+                Accept
+              </button>
+              <button onClick={() => denyFriendRequest(request)}>Deny</button>
+              <hr />
             </div>
+          ))}
         </div>
       </div>
+    </div>
   );
 }
 
