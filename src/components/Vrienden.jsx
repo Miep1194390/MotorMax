31a7aa33649fce945822209d9223e1fea59fad67
ililuab <1194420@student.roc-nijmegen.nl>
2023-06-19 11:32:19 +0200

    vrienden

--- src/components/Vrienden.jsx
+++ src/components/Vrienden.jsx
@@ -4,6 +4,8 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore, collection, query, where, getDocs, addDoc } from 'firebase/firestore';
 import { useState } from 'react';
 import { db } from '../firebase';
+import { Link } from 'react-router-dom';
+
 
 
 function Vrienden() {
@@ -50,25 +52,37 @@ function Vrienden() {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={searchInput}
-        onChange={(e) => setSearchInput(e.target.value)}
-        placeholder="Search..."
-      />
-      <button onClick={searchFirestore}>Search</button>
-      <div>
-        {searchResults.map((result) => (
-          <div key={result.id}>
-            <p>Name: {result.displayName}</p>
-            <p>Email: {result.email}</p>
-            <button onClick={() => sendFriendRequest(result.id)}>Send Friend Request</button>
-            <hr />
+    <div className="container-fluid maak-container">
+      <div className="row">
+        <div className="col-xxl-3 col-xl-3 col-lg-3 col-md-3">
+          <div className="sidebar-vrienden">
+            <Link to="/">TERUG</Link>
+            <input
+              className='mt-4'
+              type="text"
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+              placeholder="Search..."
+              />
+              <button onClick={searchFirestore} className='mt-3' >Search</button>
+            </div>
           </div>
-        ))}
+          
+
+            <div className="col-xxl-9 col-xl-9 col-lg-9 col-md-9">
+              <div className='meetings'>
+                {searchResults.map((result) => (
+                  <div key={result.id}>
+                    <p>Name: {result.displayName}</p>
+                    <p>Email: {result.email}</p>
+                    <button onClick={() => sendFriendRequest(result.id)}>Send Friend Request</button>
+                    <hr />
+                  </div>
+                ))}
+              </div>
+            </div>
+        </div>
       </div>
-    </div>
   );
 }
 
